9a1707b51e40031a2592194db934e92dc1e55d46
Divyanshu Agrawal <diagrawa@redhat.com>
2021-02-08 20:13:00 +0530

    action-io-generator: Add script debug step (#23)
    Signed-off-by: divyansh42 <diagrawa@redhat.com>

--- action-io-generator/entrypoint.sh
+++ action-io-generator/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e 
-set -o pipefail 
+set -o pipefail
 
 # Define a timestamp function
 timestamp() {
@@ -12,9 +12,12 @@ npm ci
 file_name="action-generated-ins-outs-$(timestamp).ts"
 echo "Generating inputs and outputs enum in $file_name"
 
-npx action-io-generator --outFile=$file_name
+set -x
 
+npx action-io-generator --outFile=$file_name
 git --no-pager diff --no-index --exit-code $INPUT_IO_FILE $file_name
+
+set +x
 
 if [[ $? -eq 0 ]]; then
     echo "✅ Inputs and Outputs are configured correctly!"
